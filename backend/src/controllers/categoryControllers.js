016cda1af01ffc096fa8e50659a61ce4c566bd85
imenebaraitame <baraitame.imene@gmail.com>
2026-01-06 17:59:18 +0100

    refactor: update category controllers to support authentication

--- backend/src/controllers/categoryControllers.js
+++ backend/src/controllers/categoryControllers.js
@@ -1,10 +1,5 @@
 import Category from "../models/category.js";
-import express from "express";
-import errorMiddleware from "../middlewares/errorMiddleware.js";
-const app = express();
 
-// use error middleware
-app.use(errorMiddleware);
 
 // Get all categories
 const getCategories = async(req, res) => {
@@ -17,45 +12,51 @@ const getCategories = async(req, res) => {
 };
 
 // Get category by id
-const getCategoryById = async(req, res) => {
+const getCategoryById = async(req, res, next) => {
   try {
     const category = Category.findById(req.params.id);
   
     if (!category) {
-      res.status(404).json({ message: "Category not found" });
+      const error = new Error("Category not found");
+      error.status = 404;
+      error.message = "Category not found";
+      throw error; 
     } 
     res.json(category);
   } catch (error) {
-    res.status(500).json({ message: "Server Error", error: error.message });
+    next(error);
   }
 };
 
 // Add new category
-const addCategory = async(req, res) => {
+const addCategory = async(req, res, next) => {
   try {
     const {name} = req.body;
     if (!name) {
-      return res.status(400).json({ message: "category name is required" });
+      const error = new Error("Category name is required");
+      error.status = 400;
+      error.message = "Category name is required";
+      throw error;
     }
-    const newCategory = new Category({name});
-
-    const existingCategory = await Category.findOne({ name: name });
+    const existingCategory = await Category.findOne({ name });
     if (existingCategory) {
-      return res.status(400).json({ message: "Category name must be unique" });
+      const error = new Error("Category must be unique"); 
+      error.status = 400;
+      error.message = "Category must be unique";
+      throw error;
     }
 
+    const newCategory = new Category({ name });
     const savedCategory = await newCategory.save();
+
     res.status(201).json(savedCategory);
   } catch (error) {
-    if (error.name === 'ValidationError') {
-      return res.status(400).json({ message: "Validation Error", error: error.message });
-    }
-    res.status(500).json({ message: "Server Error", error: error.message });
+    next(error);
   }
 };
 
 // Update category
-const updateCategory = async(req, res) => {
+const updateCategory = async(req, res, next) => {
   try {
     const categoryId = req.params.id;
     const {name} = req.body;
@@ -66,29 +67,32 @@ const updateCategory = async(req, res) => {
     );
 
     if (!updatedCategory) {
-      return res.status(404).json({ message: "Category not found" });
+      const error = new Error("Category not found");
+      error.status = 404;
+      error.message = "Category not found";
+      throw error; 
     }
     res.json(updatedCategory);
 
   } catch (error) {
-    if (error.name === 'ValidationError') {
-      return res.status(400).json({ message: "Validation Error", error: error.message });
-    }
-    res.status(500).json({ message: "Server Error", error: error.message });
+    next(error);
   }
 };
 
 // Delete category
-const deleteCategory = async(req, res) => {
+const deleteCategory = async(req, res, next) => {
 try {
     const categoryId = req.params.id;
     const deletedCategory= await Category.findByIdAndDelete(categoryId);
     if (!deletedCategory) {
-      return res.status(404).json({ message: "Category not found" });
+      const error = new Error("Category not found");
+      error.status = 404;
+      error.message = "Category not found";
+      throw error; 
     }
     res.json({ message: "Category deleted successfully" });
 } catch (error) {
-  res.status(500).json({ message: "Server Error", error: error.message });
+  next(error);
 }
 };
 
--- backend/src/routes/categoriesRoutes.js
+++ backend/src/routes/categoriesRoutes.js
@@ -7,14 +7,18 @@ import {
   deleteCategory,
 } from "../controllers/categoryControllers.js";
 
+import authenticationToken from '../middlewares/auth.js';
+import isAdmin from '../middlewares/isAdmin.js';
+
+
 const router = express.Router();
 
 // Define the route to get all categories
 router.get("/", getCategories); // localhost:3000/api/categories/
-router.post("/", addCategory); // localhost:3000/api/categories/
 router.get("/:id", getCategoryById); // localhost:3000/api/categories/:id
-router.put("/:id", updateCategory); // localhost:3000/api/categories/:id
-router.delete("/:id", deleteCategory); // localhost:3000/api/categories/:id
+router.post("/", authenticationToken,isAdmin, addCategory); // localhost:3000/api/categories/
+router.put("/:id", authenticationToken, isAdmin, updateCategory); // localhost:3000/api/categories/:id
+router.delete("/:id", authenticationToken, isAdmin, deleteCategory); // localhost:3000/api/categories/:id
 
 // Export the router
 export default router;
