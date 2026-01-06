3bfc80dd99fac0c867f3b12e0ed9399113f2fcd5
imenebaraitame <baraitame.imene@gmail.com>
2026-01-06 18:07:04 +0100

    add user router & setup bcrypt

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import productRoutes from './productRoutes.js';
 import categoriesRoutes from './categoriesRoutes.js';
 import cartRoutes from './cartRoutes.js';
+import userRoutes from './userRoutes.js';
 
 const router = express.Router();
 
 router.use('/api/products', productRoutes);
 router.use('/api/categories', categoriesRoutes);
 router.use('/api/carts', cartRoutes);
+router.use('/api/users', userRoutes); // http://localhost:5000/api/users/
 
 
 export default router
